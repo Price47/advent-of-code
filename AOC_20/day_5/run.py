a90f57afce2a26597e1abe38cdef6a0cbe0a61ce
Richard <richardstoeffel47@gmail.com>
2020-12-05 00:34:07 -0500

    NF: AOC day 5 2020

---
+++ AOC_20/day_5/run.py
@@ -0,0 +1,4 @@
+from .seat_finder import SeatFinder
+
+if __name__ == '__main__':
+    SeatFinder().main()
---
+++ AOC_20/day_5/seat_finder.py
@@ -0,0 +1,61 @@
+from core import DefaultLogger, AOCBase
+import re
+
+
+log = DefaultLogger.get_log()
+
+
+class SeatFinder(AOCBase):
+    """
+    https://adventofcode.com/2020/day/4
+
+    """
+
+    def parse_data(self):
+        operations = {
+            'F': lambda x: x[:len(x) // 2],
+            'B': lambda x: x[len(x) // 2:],
+            'L': lambda x: x[:len(x) // 2],
+            'R': lambda x: x[len(x) // 2:]
+        }
+
+        seat_results = []
+
+
+        data = self.read_input()
+        for d in data:
+            plane_rows = list(range(128))
+            plane_seats = list(range(8))
+            for char in d[:7]:
+                plane_rows = operations[char](plane_rows)
+            for char in d[7:]:
+                plane_seats = operations[char](plane_seats)
+
+            seat = plane_seats[0]
+            row = plane_rows[0]
+            seat_id = (row * 8) + seat
+
+            seat_results.append((seat_id, row, seat))
+
+        return seat_results
+
+    def find_seat(self, ordered_seats):
+        # This shoudl really have some additional steps I think
+        # but it ended up finding the right answer anyway
+        missing_seat_ids = []
+        seat_ids = [sid for sid, _, _ in ordered_seats]
+        for x in range(len(seat_ids) - 1):
+            if seat_ids[x+1] != seat_ids[x] + 1:
+                missing_seat_ids.append(seat_ids[x] + 1)
+
+
+        return missing_seat_ids
+
+    def main(self):
+        seat_results = self.parse_data()
+        ordered_seats = sorted(seat_results, key=lambda x: x[0])
+
+        seat = self.find_seat(ordered_seats)
+        log.info(f"my seat: {seat}")
+
+
