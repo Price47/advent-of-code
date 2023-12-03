441f66786fdcec8eded68b086af2e9a23dd9de2f
Richard <richardstoeffel47@gmail.com>
2023-12-03 13:26:35 -0500

    -RF: Cleanup

--- AOC_23/day_03/day.py
+++ AOC_23/day_03/day.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from functools import reduce
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 from core import DefaultLogger, AOCBase, AOCStr as astr
 
@@ -18,18 +18,31 @@ dirs = {
     "down-right": (1,1)
 }
 
+ENGINE_PARTS_COORIDNATE_LIST = List[Tuple[astr, Tuple[int, int]]]
+
 class day(AOCBase):
+    cur_row = None
+    cur_col = None
+
     def _engine_part_number(self, row: int, col: int, part_number_sequence: astr = "") -> astr:
         try:
             if new_part_number_sequence := self.is_int(self.data[row][col+1], astr):
-                part_number_sequence = self._engine_part_number(row, col+1, part_number_sequence + new_part_number_sequence)
+                part_number_sequence = self._engine_part_number(
+                    row, col+1, part_number_sequence + new_part_number_sequence
+                )
         except IndexError:
             return part_number_sequence
 
         return part_number_sequence
 
-    def get_engine_parts(self):
-        engine_parts: List[Tuple[astr, Tuple[int, int]]] = []
+    def _safe_r(self, row_val: int):
+        return min(max(row_val, 0), len(self.data) - 1)
+
+    def _safe_c_range(self, engine_part: astr):
+        return range(max(self.cur_col - 1, 0), min(self.cur_col + len(engine_part) + 1, len(self.data[self.cur_row])))
+
+    def get_engine_parts(self) -> ENGINE_PARTS_COORIDNATE_LIST:
+        engine_parts: ENGINE_PARTS_COORIDNATE_LIST = []
         for row in range(len(self.data)):
             col_position = 0
             while col_position < len(self.data[row]):
@@ -43,27 +56,20 @@ class day(AOCBase):
 
         return engine_parts
 
-
-    def check_engine_part_adjacency(self, row, col, engine_part, fn):
+    def engine_part_value(self, engine_part: astr) -> int:
         for row_delta in range(-1, 2):
-            r = min(max(row + row_delta, 0), len(self.data) - 1)
-            for c in range(max(col - 1, 0), min(col + len(engine_part) + 1, len(self.data[row]))):
+            r = self._safe_r(self.cur_row + row_delta)
+            for c in self._safe_c_range(engine_part):
                 char = self.data[r][c]
-                return fn(char, engine_part)
-                # if not self.is_int(char, astr) and char != ".":
-                #     return int(engine_part)
+                if not self.is_int(char, astr) and char != ".":
+                    return int(engine_part)
 
-    def engine_part_value(self, row, col, engine_part) -> int:
-        def _fn(char: str, engine_part: astr):
-            if not self.is_int(char, astr) and char != ".":
-                return int(engine_part)
+        return 0
 
-        return self.check_engine_part_adjacency(row, col, engine_part, _fn) or 0
-
-    def find_gear_pairs(self, row, col, engine_part):
+    def find_gear_pairs(self, engine_part: astr) -> Optional[Tuple[int, int]]:
         for row_delta in range(-1, 2):
-            r = min(max(row+row_delta, 0), len(self.data)-1)
-            for c in range(max(col-1, 0), min(col+len(engine_part)+1, len(self.data[row]))):
+            r = self._safe_r(self.cur_row + row_delta)
+            for c in self._safe_c_range(engine_part):
                 char = self.data[r][c]
                 if char == "*":
                     return r, c
@@ -72,7 +78,9 @@ class day(AOCBase):
         engine_parts = self.get_engine_parts()
         engine_parts_sum = 0
         for engine_part, (row, col) in engine_parts:
-            engine_parts_sum+=self.engine_part_value(row, col, engine_part)
+            self.cur_row = row
+            self.cur_col = col
+            engine_parts_sum+=self.engine_part_value(engine_part)
 
         log.info(f"[Engine Parts Sum] {engine_parts_sum}")
         assert engine_parts_sum == 512794
@@ -82,7 +90,9 @@ class day(AOCBase):
         partial_gears = defaultdict(lambda: defaultdict(list))
         engine_parts = self.get_engine_parts()
         for engine_part, (row, col) in engine_parts:
-            if found := self.find_gear_pairs(row, col, engine_part):
+            self.cur_row = row
+            self.cur_col = col
+            if found := self.find_gear_pairs(engine_part):
                 row, col = found
                 partial_gears[row][col].append(int(engine_part))
 
@@ -92,8 +102,8 @@ class day(AOCBase):
                 if len(gear_parts) == 2:
                     gear_ratio_sums += reduce(lambda x, y: x * y, gear_parts)
 
-        assert gear_ratio_sums == 67779080
         log.info(f"[Gear Ratio Sums] {gear_ratio_sums}")
+        assert gear_ratio_sums == 67779080
 
     def run(self):
         self.find_part_numbers()
